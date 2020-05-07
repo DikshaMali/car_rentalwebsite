007d1b86457e14c0f11769ee47e340d4c3f9a5bc
DikshaMali <60825523+DikshaMali@users.noreply.github.com>
2020-05-07 22:33:38 +0530

    Add files via upload

---
+++ admin/includes/leftbar.php
@@ -0,0 +1,8 @@
+	<nav class="ts-sidebar">
+			<ul class="ts-sidebar-menu">
+			
+				<li class="ts-label">Main</li>
+				
+<li><a href="manage-bookings.php"><i class="fa fa-users"></i> Manage Booking</a></li>
+<li><a href="reg-users.php"><i class="fa fa-users"></i> Reg Users</a></li>	</nav>
+<li><a href="reg.php"><i class="fa fa-users"></i> Register new admin</a></li>	</nav>
